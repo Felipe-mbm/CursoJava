318e9483ea4ddf2c866b904792f30e572b2107bf
FelipeMessaMarques <felipembmarques@gmail.com>
2025-11-19 19:29:11 -0300

    Exercicio de fixação

---
+++ src/interfaces/exercicioFixacao/model/services/ContractService.java
@@ -0,0 +1,10 @@
+package interfaces.exercicioFixacao.model.services;
+
+import interfaces.exercicioFixacao.model.entities.Contract;
+
+public class ContractService {
+
+    public void processContract(Contract contract, Integer months) {
+
+    }
+}
